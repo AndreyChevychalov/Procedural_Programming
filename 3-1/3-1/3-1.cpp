9c2f571a3d56e9aab4f7947b152b82bdaf2b97c1
AndreyChevychalov <ach1312@yandex.ru>
2021-05-26 04:51:56 +0300

    Добавлен код

---
+++ 3-1/3-1/3-1.cpp
@@ -0,0 +1,24 @@
+﻿#define _USE_MATH_DEFINES // for C++
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double function(double x);
+
+int main()
+{
+	double x;
+	for (x = 0; x <= 1; x = x + 0.1)
+	{
+		 double у = function(x);
+		 cout << "\nx= " << x << " y= " << y;
+	}
+	return 0;
+}
+
+double function(double x)
+{
+	return sqrt(1 - x) - cos(sqrt(1 - x));
+}
